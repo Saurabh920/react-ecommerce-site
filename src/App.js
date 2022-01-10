82a80abae59ae17876bbe0589ff7c003f3f14247
Saurabh920 <saurabhshah920@gmail.com>
2022-01-10 20:41:31 +0545

    .

--- src/App.js
+++ src/App.js
@@ -9,14 +9,13 @@ import Home from "./Pages/Home/Home"
 import Cart from "./Pages/Cart/Cart"
 import History from "./Pages/OrderHistory/OrderHistory";
 import CheckOut from "./Pages/CheckOut/CheckOut";
-import reactEcommerceSite from "./Pages/react-ecommerce-site/react-ecommerce-site"
 
 function App() {
 return (
     <> 
     <AnotherNavbar/>
     <Switch>
-      <Route path="/" component={reactEcommerceSite} exact />
+      <Route path="/" component={Home} exact />
       <Route path="/login" component={Login} exact />
       <Route path="/register" component={Register} exact />
       <Route path="/cart" component={Cart} exact />
